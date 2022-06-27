cc95b1f1e657c553453b616b1bc061a5f33aae13
Tal Amit <83287794+tal0311@users.noreply.github.com>
2022-06-27 13:10:31 +0300

    text draw

--- src/classesES6.js
+++ src/classesES6.js
@@ -10,12 +10,9 @@ document.getElementById("app").innerHTML = `
 var gCanvas = document.querySelector("canvas");
 var gCtx = gCanvas.getContext("2d");
 
-class shape {
-  constructor(name, width, height, fillcolor) {
-    (this.name = name),
-      (this.width = width),
-      (this.height = height),
-      (this.fillcolor = fillcolor);
+class Shape {
+  constructor(width, height, fillcolor) {
+    (this.width = width), (this.height = height), (this.fillcolor = fillcolor);
   }
 
   get description() {
@@ -25,11 +22,37 @@ class shape {
   set color(value) {
     this.fillcolor = value;
   }
-  printShape() {
-    console.log(gCtx);
+  printRect(x, y) {
+    // console.log(gCtx);
+    console.log(x, y);
+    gCtx.strokeStyle = this.fillcolor;
+    gCtx.strokeRect(x, y, this.width, this.height);
+  }
+  printTxt(txt, x, y) {
+    gCtx.font = "50px serif";
+    gCtx.strokeText(txt, x, y, gCanvas.width);
   }
 }
 
-const shape1 = new shape("rect", 100, 500, "red");
-// shape1.color = "black";
-console.log(shape1.printShape());
+// const shape1 = new shape("rect", 100, 100, "red");
+// // shape1.color = "black";
+// console.log(shape1.printShape(100, 100));
+
+class Rect extends Shape {
+  constructor(shapename, width, height, fillcolor) {
+    super(width, height, fillcolor);
+  }
+}
+
+class Txt extends Shape {
+  constructor(shapename, width, height, fillcolor) {
+    super(width, height, fillcolor);
+  }
+}
+
+const txt1 = new Txt("txt", 100, 100, "black");
+console.log(txt1);
+txt1.printTxt("hello world", 100, 250);
+
+const rect1 = new Rect("rect", 100, 100, "red");
+console.log(rect1.printRect(100, 100));
